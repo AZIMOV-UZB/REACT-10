603750eedaad585fde44c13967b0046bc271e565
AZIMOV-UZB <abduvorisazimov787@gmail.com>
2024-08-20 22:42:01 -0700

    react

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -2,6 +2,7 @@ import { useStateValue } from "@/context/Index";
 import React from "react";
 import { FaHeart } from "react-icons/fa";
 import { toast } from "react-toastify";
+import { FaStar } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 const Products = ({ data, title }) => {
   const navigate = useNavigate();
@@ -16,9 +17,16 @@ const Products = ({ data, title }) => {
         />
       </div>
       <div className="flex flex-col my-2  	gap-2">
-        <p>{product.title}</p>
-        <strong>{product.price} USD</strong>
-        <div className="absolute top-[20px] right-[20px] text-2xl text-white">
+        <p className="text-xl font-bold">{product.title}</p>
+        <div className="flex items-center	px-2 gap-1">
+        <FaStar className="text-yellow-300" />
+        <p>
+        {product.rating}</p>
+        </div>
+        <p className="bg-yellow-300 w-32  rounded
+
+	px-3">{product.price} USD</p>
+        <div className="absolute top-[20px] right-[20px] text-xl text-white">
           <button>
             <FaHeart
               onClick={() => {
@@ -26,7 +34,7 @@ const Products = ({ data, title }) => {
                 toast.success("Mahsulot  qo'shildi", {position: "bottom-right"
                 });
               }}
-              className="text-50px hover:text-red-600"
+              className="text-50px ease-in-out	 hover:text-red-600"
             />
           </button>
         </div>
@@ -39,7 +47,7 @@ const Products = ({ data, title }) => {
             });
           }}
         >
-          add cart
+          Add Card
         </button>
       </div>
     </div>
